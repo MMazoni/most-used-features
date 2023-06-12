a06957da5aaf5daa4009c8e470f46c4d67ad183b
Matheus Mazoni <mmazoni.andrade@gmail.com>
2023-06-12 11:42:07 -0300

    Get the input as a prompt by the user

--- internal/input/get_input.go
+++ internal/input/get_input.go
@@ -1,13 +1,20 @@
 package input
 
 import (
+    "bufio"
+    "fmt"
     "log"
+    "strings"
     "os"
 )
 
-const filePath = "/var/log/httpd/sbx-us_access_log"
-
 func GetInput() *os.File {
+    reader := bufio.NewReader(os.Stdin)
+
+    fmt.Print("Enter the log file path: ")
+    filePath, _ := reader.ReadString('\n')
+    filePath = strings.TrimSpace(filePath)
+
     file, err := os.Open(filePath)
     if err != nil {
         log.Fatal(err)
